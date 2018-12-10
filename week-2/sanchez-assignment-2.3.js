d0aa54b717ea84d239bea4788d3b530f1142eb4d
Gabriel Sanchez <gjsanchez@my365.bellevue.edu>
2018-12-09 18:44:54 -0600

    Week 2

---
+++ week-2/sanchez-assignment-2.3.js
@@ -0,0 +1,32 @@
+/*
+
+Title: sanchez-assignment-2.2.js
+Date: 11/29/2018
+Author: Profesor Krasso
+Modified by: Gabriel Sanchez
+Description: Assignment 2.2
+*/
+
+
+/* header */
+var header = require ('../sanchez-header.js');
+console.log(header.display('Gabriel', 'Sanchez', 'assignment 2.2'));
+
+// Code here..
+// Create a function with myName, with 0 parameters and empty body
+
+// Function property
+myName.Gabriel = "Hello Gabriel Sanchez";
+
+
+// Function
+
+function myName () {
+    return myName.Gabriel;
+}
+
+// call function
+
+console.log('\n');
+console.log( myName());
+
